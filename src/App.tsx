f7ec6e39110889534e87dc6508cdffea6a6323cf
TruePacifism <76737760+TruePacifism@users.noreply.github.com>
2024-01-25 13:55:55 +0300

    Изменил иконки на карте
    - Сбросился их функционал

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
+import { YMaps } from "@pbe/react-yandex-maps";
 import "./App.css";
 import MyMap from "./Map/Map";
 import WritersList from "./WritersList/WritersList";
 
 function App() {
   return (
-    <div className="App">
-      <footer className="footer"></footer>
-      <main className="main">
-        <WritersList />
-        <MyMap />
-      </main>
-    </div>
+    <YMaps
+      query={{
+        apikey: "68bf9d85-0ca9-461c-8405-422ed94b196d",
+        lang: "ru_RU",
+      }}
+    >
+      <div className="App">
+        <footer className="footer"></footer>
+        <main className="main">
+          <WritersList />
+          <MyMap />
+        </main>
+      </div>
+    </YMaps>
   );
 }
 
--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -7,16 +7,30 @@ import React, {
   useState,
 } from "react";
 import styles from "./Map.module.css";
-import { Map, Placemark, YMaps } from "@pbe/react-yandex-maps";
+import { Map, Placemark } from "@pbe/react-yandex-maps";
 import { useDispatch, useSelector } from "react-redux";
 import { stateType, writerInfo } from "../utils/types";
 import WritersListItem from "../WritersListItem/WritersListItem";
 import { selectWriterAction } from "../utils/store";
-
+import { ReactComponent as MapIcon } from "../images/icons/map-pin.svg";
+import { useYMaps } from "@pbe/react-yandex-maps";
+import ReactDOMServer from "react-dom/server";
 export default function MyMap() {
   const writers = useSelector<stateType, writerInfo[]>(
     (store) => store.writers
   );
+  const ymaps = useYMaps(["templateLayoutFactory", "Placemark"]);
+  const icon = ymaps?.templateLayoutFactory?.createClass(
+    `${ReactDOMServer.renderToStaticMarkup(
+      <MapIcon
+        className={styles.placemark}
+        onClick={() => {
+          console.log("rer");
+        }}
+      />
+    )}`
+  );
+
   const dispatch = useDispatch();
   const [popupX, setPopupX]: [number, Dispatch<SetStateAction<number>>] =
     useState(0);
@@ -34,46 +48,39 @@ export default function MyMap() {
   const popupRef = useRef<HTMLDivElement>(null);
   return (
     <>
-      <YMaps
-        query={{
-          apikey: "68bf9d85-0ca9-461c-8405-422ed94b196d",
-          lang: "ru_RU",
-        }}
+      <Map
+        state={{ center: centerCoords, zoom: 3 }}
+        className={styles.mapContainer}
       >
-        <Map
-          state={{ center: centerCoords, zoom: 3 }}
-          className={styles.mapContainer}
-        >
-          {writers.map((writer) => (
-            <Placemark
-              geometry={writer.coordinates}
-              onMouseEnter={(e: any) => {
-                const event: MouseEvent =
-                  e.originalEvent.domEvent.originalEvent;
-                console.log(e);
+        {writers.map((writer) => (
+          <Placemark
+            geometry={writer.coordinates}
+            onMouseEnter={(e: any) => {
+              const event: MouseEvent = e.originalEvent.domEvent.originalEvent;
+              console.log(e);
 
-                setPopupContent(<WritersListItem writerInfo={writer} />);
-                // const popupRect = popupRef.current?.getBoundingClientRect();
-                // const popupHeight = popupRect?.height;
-                // const popupWidth = popupRect?.width;
-                // console.log(popupRect);
+              setPopupContent(<WritersListItem writerInfo={writer} />);
+              // const popupRect = popupRef.current?.getBoundingClientRect();
+              // const popupHeight = popupRect?.height;
+              // const popupWidth = popupRect?.width;
+              // console.log(popupRect);
 
-                const x: number = event.clientX - 20;
-                const y: number = event.clientY - 20;
-                setPopupX(x);
-                setPopupY(y);
-                setIsPopupShowing(true);
-              }}
-              onMouseLeave={(e: any) => {
-                setIsPopupShowing(false);
-              }}
-              onClick={(e: any) => {
-                dispatch(selectWriterAction(writer));
-              }}
-            />
-          ))}
-        </Map>
-      </YMaps>
+              const x: number = event.clientX - 20;
+              const y: number = event.clientY - 20;
+              setPopupX(x);
+              setPopupY(y);
+              setIsPopupShowing(true);
+            }}
+            onMouseLeave={(e: any) => {
+              setIsPopupShowing(false);
+            }}
+            onClick={(e: any) => {
+              dispatch(selectWriterAction(writer));
+            }}
+            options={{ iconLayout: icon }}
+          />
+        ))}
+      </Map>
       <div
         className={styles.popup}
         style={{
